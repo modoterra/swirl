631ef256832f14180a6f9e92557498612c815bb6
Christoffer Hallas <git@christofferhallas.com>
2024-11-23 19:31:51 -0500

    feat: added additional options
    Added option for a clean install that removes traces of SWIRL. Also added an option for an install with no prompts.
    
    Refs: #8

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel from 'laravel-vite-plugin'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.tsx'],
-            refresh: true,
-        }),
-    ],
-});
+  plugins: [
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.tsx'],
+      refresh: true
+    })
+  ]
+})
